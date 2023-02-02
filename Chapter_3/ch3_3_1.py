1ada3d7b7a7d4ebf2555a60f7de3f393b5f5c17f
Edison <edisonstuart@gmail.com>
2023-02-01 21:03:08 -0500

    WIP: storing three stacks in an array

--- Chapter_3/ch3_3_1.py
+++ Chapter_3/ch3_3_1.py
@@ -182,12 +182,170 @@ popFromBackStack will mirror popFromFrontStack
 We will call this modified calss ArrayOfStacks
 
 	}
+
+Now in order to change this ArrayOfStacks class to work with
+three stacks we have to add a stack in the middle of the array.
+To do this we have to keep track of
+1) the head of the stack and
+2) the tail of the stack.
+We have to do this because our front stack and back stack have tails
+at the start of the aray and the end of the array respectively, but
+the middle stack does not have this built in end.
+
+We will also rename head_1 and head_2 to head_front and head_back
+as for now, we will not store the head pointers in an array, and accept a variable
+array size that will determine the size of the array which our stacks will be housed in.
+
+    constructor(array_size) {
+    		self.stack = [null] * array_size
+    		self.head_back = null
+            self.head_front = null
+            self.middle[floor(array_size / 2), floor(array_size / 2)] // head, tail
+    	}
+
+We can allocate space for our stacks dynamically
+by shifting the middle stack over when there is an
+overlap about to happen.
+
+The code for pushing to the front or back stack will
+now have to include a check to make sure there is space
+for the stack to grow.
+
+    _push_to_front_stack(self, data) {
+
+We do the same check to see if the front head is null
+
+        if (self.head_front is null) {
+            self.head_front = 0
+            self.stack[self.head_front] = data
+        }
+
+Then we check if the head would overlap with the middle if added to, if so,
+we also check if the middle would overlap with the back if moved, if so,
+we then raise an execption because there is no more space left in the array
+
+        elif (self.head_front + 1 == self.middle[1] && self.middle[0] + 1 == self.head_back) {
+            raise Exception("No more space in array")
+        }
+
+If the above check does not return true, but the front stack will
+still overlap, we will then shift the middle stack the opposite direction
+the front stack is growing. We will write a new function to do this called
+_shift_middle.
+
+        elif (self.head_front + 1 == self.middle[1]) {
+            self._shift_middle(1)
+        }
+
+After we have made sure there is enough space, we can then
+add 1 to the front head value, then use that index to put
+our data into the array.
+
+        self.head_front += 1
+        self.stack[self.head_front] = data
+    }
+
+We have to make similar changes to _push_to_back_stack.
+
+    def _push_to_back_stack(self, data) {
+
+
+We start by doing the same if head is null check
+
+
+        if (self.head_back is null) {
+            self.head_back = len(self.stack) - 1
+            self.stack[self.head_back] = data
+        }
+
+We do the same check for space in the opposite direction
+
+        elif (self.head_back -1 == self.middle[1] && self.middle[0] - 1 == self.head_front) {
+            raise Exception("No more space in array")
+        }
+
+Then if there is no exception, we again check for collisions,
+and move the middle if needed
+
+        elif (self.head_back - 1 == self.middle[1]) {
+            self._shift_middle(-1)
+        }
+
+After these checks have passed and space is available,
+we add to the stack like before.
+
+        self.head_back -= 1
+        self.stack[self.head_back] = data
+    }
+
+Now that we have made these changes to pushing from both sides,
+lets implement a function that will simply shift the middle stack.
+We could make it so the stack is put in between the open space that is left
+in order to decrease the amount of times this operation needs to run.
+For now we will just move the middle stack by 1.
+
+
+    _shift_middle(self, direction) {
+
+First we should check which direction we are shifting
+
+        if (direction < 0) {
+
+if the direction is negative, we shift left.
+We shift left by looping through the range from the tail
+node, middle[0], and the head node, middle[1] + 1
+
+            for (i in range(self.middle[0], self.middle[1] + 1)) {
+            self.stack[i + direction] = self.stack[i]
+        }
+
+After that, we want to shift the head and tail pointer back 1 as well.
+
+            self.middle[0] -= 1
+            self.middle[1] -= 1
+        }
+
+        elif (direction > 0) {
+
+if the direction is positive, we shift right.
+We shift right by again, looping through the range; but this time
+reversed.
+
+            reversed_range = reversed(range(self.middle[0], self.middle[1] + 1))
+
+with the reversed range, we can do the same translation as before
+
+            for (i in reversed_range) {
+            self.stack[i + direction] = self.stack[i]
+            }
+
+After we loop, we then increase the pointer value for the
+middle head and tail.
+
+            self.middle[0] += 1
+            self.middle[1] += 1
+        }
+    }
+
+
+With the hard part out of the way, now we just have to implement
+the _push_to_middle function, and then the pop function.
+
+
+for pushing to the middle stack, we will first check if the middle is empty.
+If so we then do a similar check to the last functions for head collisions,
+moving the middle stack if needed.
+
+    _push_to_middle(self, data) {
+
+    }
+
 '''
 class ArrayOfStacks:
     '''A class that uses an array which stores a stack in a subset of the array'''
     def __init__(self, stack_size):
         '''Accepts size limit of stack, no greater than 9'''
-        self.stack = [None] * 10
+        self.stack = [None] * 20
         self.head_1 = None
         self.head_2 = None
         self.stack_size = stack_size
@@ -212,6 +370,17 @@ class ArrayOfStacks:
             value = self._pop_from_back_stack()
         else:
             raise Exception("value 'stack' should be 1 or 2")
+        return value
+
+    def peek(self, stack):
+        '''Method accepts 1 or 2, denoting which stack
+		   you want to access. Then returns the top value
+		   from that stack'''
+        value = None
+        if stack == 1:
+            value = self.stack[self.head_1]
+        elif stack == 2:
+            value = self.stack[self.head_2]
         return value
 
     def _push_to_front_stack(self, data):
@@ -273,3 +442,5 @@ if __name__ == "__main__":
     my_stack.push(1, 2)
     my_stack.push(7, 1)
     my_stack.push(100, 1)
+    print(my_stack.peek(2))
+    print(my_stack.peek(1))
