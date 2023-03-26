5c97e52384ce27f510fb6f085a1d709d4abc6c79
Edison <edisonstuart@gmail.com>
2023-03-25 20:05:35 -0400

    Changed solution to 4.12 to work with different data structure

--- Chapter_4/ch4_4_12.py
+++ Chapter_4/ch4_4_12.py
@@ -6,14 +6,19 @@ to start at the root or end at a leaf but it must only travel downwards.
 
 from copy import deepcopy
 
+class Node:
+    def __init__(self, data, index):
+        self.data = data
+        self.index = index
+
 class CheckForPath:
     '''
     Args:
         adjacency_list(dict): A dictionary of nodes and what they are adjacent to.
     '''
-    def __init__(self, adjacency_list: dict):
+    def __init__(self, adjacency_list: list):
         self.adjacency_list = adjacency_list
-        self.vertices = self.adjacency_list.keys()
+        self.vertices = {item[0]: item[1] for item in adjacency_list}.keys()
         self.paths = []
 
     def find_paths_with_sum(self, target: int) -> list:
@@ -36,52 +41,74 @@ class CheckForPath:
 
     def rebuild_paths(self):
         """Rebuild all paths for the graph"""
-        try:
-            self.paths = []
-            for source in self.vertices:
-                for target in self.vertices:
-                    self.get_paths(source, target)
+        self.paths = []
+        for source in self.vertices:
+            for target in self.vertices:
+                self.get_paths(source, target)
 
-        except Exception as error:
-            raise Exception('While rebuilding paths: %s' % error)
 
     def get_paths(self, source, target, visited=None, target_path=None):
         """Obtains list of all paths from u to v, via DFS"""
-        try:
-            if visited is None:
-                local_visited = {}
-                for vertex in self.vertices:
-                    local_visited[vertex] = False
-            else:
-                local_visited = deepcopy(visited)
+        if visited is None:
+            local_visited = {}
+            for vertex in self.vertices:
+                local_visited[vertex.index] = False
+        else:
+            local_visited = deepcopy(visited)
 
-            if target_path is None:
-                local_path = []
-            else:
-                local_path = deepcopy(target_path)
+        if target_path is None:
+            local_path = []
+        else:
+            local_path = deepcopy(target_path)
 
-            if set(local_path).intersection(set(self.adjacency_list[source])):
-                raise Exception(
-                    'Cycle present in path; ' +
-                    'source %s neighbors %s intersect path %s' %
-                    (source, self.adjacency_list[source], local_path)
-                )
 
-            local_visited[source] = True
-            local_path.append(source)
+        local_visited[source.index] = True
+        local_path.append(source.data)
 
-            if source == target:
-                self.paths.append((local_path, sum(local_path)))
-            else:
-                if self.adjacency_list[source] is not None:
-                    for i in self.adjacency_list[source]:
-                        if local_visited[i] is False:
-                            self.get_paths(i, target, local_visited, local_path)
+        if source == target:
+            self.paths.append((local_path, sum(local_path)))
+        else:
+            if self.adjacency_list[source.index][1] is not None:
+                for i in self.adjacency_list[source.index][1]:
+                    if local_visited[i.index] is False:
+                        self.get_paths(i, target, local_visited, local_path)
 
-        except Exception as error:
-            raise Exception('While obtaining paths: %s' % error)
+
+def node_generator(data):
+    index = -1
+    while index < len(data):
+        index += 1
+        yield Node(data[index], index)
+
+
+def create_node_list(node_data):
+    return_list = [[None, []]] * len(node_data)
+    my_gen = node_generator(node_data)
+    for i in range(len(return_list)):
+        next_node = next(my_gen)
+        return_list[i] = [next_node, []]
+
+    for i, node in enumerate(return_list):
+        if (i * 2) + 1 == len(return_list) - 1:
+            node[1].append(return_list[(i * 2) + 1][0])
+        elif (i * 2) + 1 > len(return_list) - 1:
+            break
+        elif i == 0:
+            node[1].append(return_list[i + 1][0])
+            node[1].append(return_list[i + 2][0])
+        elif i == 1:
+            node[1].append(return_list[i + 2][0])
+            node[1].append(return_list[i + 3][0])
+        else:
+            node[1].append(return_list[(i * 2) + 1][0])
+            node[1].append(return_list[(i * 2) + 2][0])
+
+    return return_list
+
+
+
 
 if __name__ == "__main__":
-    my_graph = CheckForPath()
-    print(my_graph.find_paths_with_sum(15))
-    # print(my_graph.paths)
+    my_list = create_node_list([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
+    my_tree = CheckForPath(my_list)
+    print(my_tree.find_paths_with_sum(15))
--- Chapter_4/tests/test_ch4_4_12.py
+++ Chapter_4/tests/test_ch4_4_12.py
@@ -1,25 +1,8 @@
 import pytest
-from ..ch4_4_12 import CheckForPath
+from ..ch4_4_12 import CheckForPath, create_node_list
 
 @pytest.mark.parametrize("target,expected", [(15, [[1, 2, 4, 8], [2, 4, 9], [5, 10], [15]])])
 def test_find_all_paths_with_sum(target, expected):
-    adjacency_list = {
-        1: [2,3],
-        2: [4,5],
-        3: [6,7],
-		4: [8,9],
-		5: [10,11],
-		6: [12, 13],
-		7: [14, 15],
-		8: [16],
-		9: [],
-		10: [],
-		11: [],
-		12: [],
-		13: [],
-        14: [],
-        15: [],
-        16: []
-        }
+    adjacency_list = create_node_list([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
     my_tree = CheckForPath(adjacency_list)
     assert my_tree.find_paths_with_sum(target) == expected
